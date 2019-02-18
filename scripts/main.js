0e33f1383d03141a5f4a3f9c6f716ca29c20b6d6
Carter King <carterking002@gmail.com>
2019-02-18 17:39:03 -0600

    refactors except parens

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,10 @@
-function add(x, y) {
-    return parseFloat(x) + parseFloat(y);
-}
-
-function subtract(x, y) {
-    return parseFloat(x) - parseFloat(y);
-}
-
-function multiply(x, y) {
-    return parseFloat(x) * parseFloat(y);
-}
-
-function divide(x, y) {
-    return parseFloat(x) / parseFloat(y);
-}
-
-function operate(a, x, y) {
-    if (a === "+") {
-        return add(x, y);
-    } else if (a === "-") {
-        return subtract(x, y);
-    } else if (a === "*") {
-        return multiply(x, y);
-    } else if (a === "/") {
-        return divide(x, y);
-    } else {
-        return;
-    }
-}
-
 const outputArea = document.getElementById("output");
 const answerArea = document.getElementById("answer-area");
 const btns = document.querySelectorAll("button");
+const operatorBtns = document.querySelectorAll(".operator");
+const numberBtns = document.querySelectorAll(".number");
+const equalBtn = document.getElementById("equals");
+const periodBtn = document.getElementById("period");
 let currentEntry;
 let operator;
 let currentLog = [];
@@ -41,7 +15,8 @@ let replacement;
 
 btns.forEach((button) => {
     button.addEventListener("click", function () {
-        if (this.classList.contains("number")) {
+        //handle numbers
+        if (this.classList.contains("number") || this.classList.contains("period")) {
             if (currentEntry == null) {
                 currentEntry = this.value;
             } else {
@@ -52,6 +27,7 @@ btns.forEach((button) => {
             } else {
                 outputArea.textContent = currentEntry;
             }
+            //handle operators
         } else if (this.classList.contains("operator")) {
             if (answer == null) {
                 currentLog.push(parseFloat(currentEntry));
@@ -59,9 +35,11 @@ btns.forEach((button) => {
             } else {
                 currentLog.splice(0, currentLog.length, answer);
                 answer = null;
+                answerArea.textContent = answer;
             }
             currentLog.push(this.value);
             outputArea.textContent = currentLog.join("");
+            //run calculations when user clicks equals button
         } else if (this.classList.contains("equals")) {
             if (answer == null) {
                 currentLog.push(parseFloat(currentEntry));
@@ -93,6 +71,7 @@ btns.forEach((button) => {
             } else answer = currentLog.join("");
             answerArea.textContent = answer;
             currentLog.splice(0, currentLog.length, answer);
+            //clear everything when user clicks "C" button
         } else if (this.classList.contains("clear")) {
             currentEntry = null;
             currentLog = [];
@@ -100,5 +79,83 @@ btns.forEach((button) => {
             outputArea.textContent = currentLog;
             answerArea.textContent = answer;
         }
+        validateButtons();
+        document.getElementById("current-entry").textContent = `currentEntry =${currentEntry}`;
+        document.getElementById("current-log").textContent = `cuurentLog = ${currentLog}`;
     })
 })
+
+function validateButtons() {
+    //disable operator buttons
+    if (currentEntry == null && answer == null) {
+        operatorBtns.forEach((opbtn) => {
+            disableBtn(opbtn);
+        })
+    } else operatorBtns.forEach((opbtn) => {
+        enableBtn(opbtn);
+    })
+    //disable equal button
+    if (currentLog.length > 1 && operator == null && currentEntry != null) {
+        enableBtn(equalBtn);
+    } else {
+        disableBtn(equalBtn);
+    }
+    //disable number buttons
+    if (answer == null) {
+        numberBtns.forEach((numBtns) => {
+            enableBtn(numBtns);
+        })
+    } else {
+        numberBtns.forEach((numBtns) => {
+            disableBtn(numBtns)
+        })
+    }
+    //disable period
+    if (currentEntry != null && currentEntry.indexOf(".") > -1) {
+        disableBtn(periodBtn);
+    } else if (answer != null) {
+        disableBtn(periodBtn);
+    } else {
+        enableBtn(periodBtn);
+    }
+}
+
+function add(x, y) {
+    return parseFloat(x) + parseFloat(y);
+}
+
+function subtract(x, y) {
+    return parseFloat(x) - parseFloat(y);
+}
+
+function multiply(x, y) {
+    return parseFloat(x) * parseFloat(y);
+}
+
+function divide(x, y) {
+    return parseFloat(x) / parseFloat(y);
+}
+
+function operate(a, x, y) {
+    if (a === "+") {
+        return add(x, y);
+    } else if (a === "-") {
+        return subtract(x, y);
+    } else if (a === "*") {
+        return multiply(x, y);
+    } else if (a === "/") {
+        return divide(x, y);
+    } else {
+        return;
+    }
+}
+
+function disableBtn(btnToDisable) {
+    btnToDisable.disabled = true;
+}
+
+function enableBtn(btnToEnable) {
+    btnToEnable.disabled = false;
+}
+
+validateButtons();
